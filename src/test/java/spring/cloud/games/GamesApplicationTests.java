8d6a325cf1f8368994968728ce89875a8e989f73
Abhishek Dujari <abhishek.dujari@gmail.com>
2018-03-24 16:08:07 +0800

    Use Discovery with stubrunner
    Signed-off-by: Abhishek Dujari <abhishek.dujari@gmail.com>

--- src/test/java/spring/cloud/games/GamesApplicationTests.java
+++ src/test/java/spring/cloud/games/GamesApplicationTests.java
@@ -6,14 +6,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = GamesApplication.class,
-    webEnvironment = WebEnvironment.NONE)
+@SpringBootTest(classes = GamesApplication.class)
 @AutoConfigureStubRunner(ids = {"spring.cloud:games:+:stubs:6565"}, workOffline = true)
 @DirtiesContext
 public class GamesApplicationTests {
